2cea0f6f03112a055928dbb89c27b14af8d188bc
i3cheese <i3cheese@yandex.ru>
2022-09-07 17:07:41 +0300

    lspconfig

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -11,7 +11,7 @@ local function on_attach(client, bufnr)
 	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 end
 
-local setup_lsp = function(capabilities)
+local function setup_lsp_installer()
 	local lsp_installer = require("nvim-lsp-installer")
 
 	lsp_installer.settings({
@@ -23,56 +23,59 @@ local setup_lsp = function(capabilities)
 			},
 		},
 	})
-
-	lsp_installer.on_server_ready(function(server)
-		local opts = {
-			on_attach = on_attach,
-			capabilities = capabilities,
-			flags = {
-				debounce_text_changes = 150,
-			},
-			settings = {},
-		}
-		if server.name == "pyright" then
-			opts.on_init = function(client)
-				client.config.settings.python.pythonPath =
-					require("core.utils").locate_python_executable(client.config.root_dir)
-			end
-		elseif server.name == "html" then
-			opts.filetypes = { "html", "htmldjango" }
-		elseif server.name == "sumneko_lua" then
-			local runtime_path = vim.split(package.path, ";")
-			table.insert(runtime_path, "lua/?.lua")
-			table.insert(runtime_path, "lua/?/init.lua")
-			opts.settings = {
-				Lua = {
-					runtime = {
-						version = "LuaJIT",
-						path = runtime_path,
-					},
-					completion = {
-						callSnippet = "Replace",
-					},
-					diagnostics = {
-						enable = true,
-						globals = { "vim", "use" },
-					},
-					workspace = {
-						library = vim.api.nvim_get_runtime_file("", true),
-						maxPreload = 10000,
-						preloadFileSize = 10000,
-					},
-					telemetry = { enable = false },
-				},
-			}
-		end
-
-		server:setup(opts)
-		vim.cmd([[ do User LspAttachBuffers ]])
-	end)
+	lsp_installer.setup({})
 end
 
-M.setup = function()
+local function setup_pyright(opts)
+	opts = vim.deepcopy(opts)
+	opts.on_init = function(client)
+		client.config.settings.python.pythonPath =
+			require("core.utils").locate_python_executable(client.config.root_dir)
+	end
+	require("lspconfig").pyright.setup(opts)
+end
+
+local function setup_html(opts)
+	opts = vim.deepcopy(opts)
+	opts.filetypes = { "html", "htmldjango" }
+	require("lspconfig").html.setup(opts)
+end
+
+local function setup_lua(opts)
+	opts = vim.deepcopy(opts)
+	local runtime_path = vim.split(package.path, ";")
+	table.insert(runtime_path, "lua/?.lua")
+	table.insert(runtime_path, "lua/?/init.lua")
+	opts.settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+				path = runtime_path,
+			},
+			completion = {
+				callSnippet = "Replace",
+			},
+			diagnostics = {
+				enable = true,
+				globals = { "vim", "use" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				maxPreload = 10000,
+				preloadFileSize = 10000,
+			},
+			telemetry = { enable = false },
+		},
+	}
+	require("lspconfig").sumneko_lua.setup(opts)
+end
+
+local function setup_cpp(opts)
+	opts = vim.deepcopy(opts)
+	require("lspconfig").clangd.setup(opts)
+end
+
+local function setup_servers()
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
 	capabilities.textDocument.completion.completionItem.documentationFormat = { "markdown", "plaintext" }
 	capabilities.textDocument.completion.completionItem.snippetSupport = true
@@ -89,19 +92,26 @@ M.setup = function()
 			"additionalTextEdits",
 		},
 	}
-	require("plugins.configs.others").lsp_handlers()
-	setup_lsp(capabilities)
-
-	require("core.mappings").lspconfig()
-	lspconfig = require("lspconfig")
-	lspconfig.clangd.setup({
+	local opts = {
 		on_attach = on_attach,
 		capabilities = capabilities,
 		flags = {
 			debounce_text_changes = 150,
 		},
 		settings = {},
-	})
+	}
+	setup_pyright(opts)
+	setup_html(opts)
+	setup_lua(opts)
+	setup_cpp(opts)
+end
+
+M.setup = function()
+	require("plugins.configs.others").lsp_handlers()
+	setup_lsp_installer()
+	setup_servers()
+
+	require("core.mappings").lspconfig()
 end
 
 return M
